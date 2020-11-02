6c060b6abc6a6db64ba2aba1a8002ea6fbc52378
Farhad Hossain <farhadhossain0085@gmail.com>
2020-11-02 15:54:25 +0600

    notes.jsx use context hooks

--- src/components/notes.jsx
+++ src/components/notes.jsx
@@ -1,34 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Note from './note'
 import { NoteContext } from '../contexts/Note.context'
 import { Link } from 'react-router-dom'
 
-class Notes extends React.Component {
+const Notes = () => {
+    const context = useContext(NoteContext)
+    const { notes } = context;
 
-    static contextType = NoteContext;
-
-    render() {
-
-        const { notes } = this.context;
-
-        return (
-            <div className="my-4">
-                <h3 className="capitalize">All notes ({notes.length}) <span className="float-right"><Link to="/add" className="btn btn-secondary">Add</Link></span></h3>
-                <hr />
-                {!notes.length ? <h4 className="text-center capitalize">No notes found</h4> : null}
-                {notes.map(note => {
-                    return (
-                        <Note
-                            id={note.id}
-                            title={note.title}
-                            key={note.id}
-                            description={note.description}
-                        />
-                    )
-                })}
-            </div>
-        )
-    }
+    return (
+        <div className="my-4">
+            <h3 className="capitalize">All notes ({notes.length}) <span className="float-right"><Link to="/add" className="btn btn-secondary">Add</Link></span></h3>
+            <hr />
+            {!notes.length ? <h4 className="text-center capitalize">No notes found</h4> : null}
+            {notes.map(note => {
+                return (
+                    <Note
+                        id={note.id}
+                        title={note.title}
+                        key={note.id}
+                        description={note.description}
+                    />
+                )
+            })}
+        </div>
+    )
 }
+
 
 export default Notes
