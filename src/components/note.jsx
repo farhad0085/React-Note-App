a3cf5bae5c03d601001ddf7579b14cb87942f815
Farhad Hossain <farhadhossain0085@gmail.com>
2020-11-02 15:51:44 +0600

    note.jsx use context hooks

--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NoteContext } from '../contexts/Note.context'
 
-class Note extends React.Component {
+const Note = (props) => {
 
-    static contextType = NoteContext;
+    const context = useContext(NoteContext)
 
-    render() {
-        const { removeNote } = this.context
-        const { id, title, description } = this.props
+    const { removeNote } = context
+    const { id, title, description } = props
 
-        return (
-            <div className="card my-2">
-                <div className="card-body">
-                    <div className="row">
-                        <div className="col-1">
-                            <button className='btn btn-danger' onClick={() => removeNote(id)}>X</button>
-                        </div>
-                        <div className="col-11">
-                            <h3>{title}</h3>
-                            <p>{description}</p>
-                        </div>
+    return (
+        <div className="card my-2">
+            <div className="card-body">
+                <div className="row">
+                    <div className="col-1 col-lg-1 col-md-1 col-sm-1">
+                        <button className='btn btn-sm btn-danger' onClick={() => removeNote(id)}>X</button>
+                    </div>
+                    <div className="col-11  col-lg-11 col-md-11 col-sm-1">
+                        <h3>{title}</h3>
+                        <p>{description}</p>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
 
 export default Note
